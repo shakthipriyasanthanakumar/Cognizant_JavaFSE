c0a625140931d4958be66fd000c02bebef2c1605
ShakthipriyaSanthanakumar <shakthipriyasanthanakumar@gmail.com>
2025-06-18 15:59:23 +0530

    changed folder name

--- week1/Design_Patterns_and_Principles/SingletonPatternExample/src/Main.java
+++ week1/Design_Patterns_and_Principles/SingletonPatternExample/src/Main.java
@@ -3,11 +3,11 @@ class Main{
     public static void main(String[] args) {
 
         Logger logger1 = Logger.getInstance(); 
-        logger1.log("First message");
+        logger1.log("First message.");
 
         Logger logger2 = Logger.getInstance(); 
-        logger2.log("Second message");
+        logger2.log("Second message.");
 
-        System.out.println("Are both loggers same? " + (logger1 == logger2));
+        System.out.println("Are both loggers same? " + (logger1 == logger2)+".");
     }
 }

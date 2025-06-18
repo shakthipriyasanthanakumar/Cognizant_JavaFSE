30136b6a7f0d3af6fd6d6ba438905ec4d87e51df
ShakthipriyaSanthanakumar <shakthipriyasanthanakumar@gmail.com>
2025-06-18 16:05:36 +0530

    organised structure

---
+++ week1/Algorithms_Data_Structures/EcommercePlatformSearchFunctionExcercise2/src/Main.java
@@ -0,0 +1,5 @@
+package Cognizant_JavaFSE.week1.Algorithms_Data_Structures.EcommercePlatformSearchFunctionExcercise2.src;
+
+public class Main {
+    
+}
---
+++ week1/Design_Patterns_and_Principles/FactoryMethodPatternExample/src/Main.java
@@ -0,0 +1,4 @@
+package Cognizant_JavaFSE.week1.Design_Patterns_and_Principles.FactoryMethodPatternExample.src;
+class Main{
+
+}

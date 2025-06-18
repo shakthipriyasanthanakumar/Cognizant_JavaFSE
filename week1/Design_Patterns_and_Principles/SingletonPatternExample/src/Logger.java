7b8592dff11c5f32dfab695ad0423de8dddbfa16
Shakthipriya.S <shakcs126@rmkcet.ac.in>
2025-06-18 15:47:10 +0530

    SingletonPatternExample completed

---
+++ week1/Design_Patterns_and_Principles/SingletonPatternExample/src/Logger.java
@@ -0,0 +1,22 @@
+package Cognizant_JavaFSE.week1.Design_Patterns_and_Principles.SingletonPatternExample.src;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger initialized.");
+    }
+
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger(); 
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
---
+++ week1/Design_Patterns_and_Principles/SingletonPatternExample/src/Main.java
@@ -0,0 +1,13 @@
+package Cognizant_JavaFSE.week1.Design_Patterns_and_Principles.SingletonPatternExample.src;
+class Main{
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance(); 
+        logger1.log("First message");
+
+        Logger logger2 = Logger.getInstance(); 
+        logger2.log("Second message");
+
+        System.out.println("Are both loggers same? " + (logger1 == logger2));
+    }
+}
